9c6a1faf4aeb968a65e29e0e79d57898661775da
VladOsiichuk <vladdosiichuk@gmail.com>
2019-04-23 13:00:23 +0300

    configuring redis

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
-
+import redis
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
