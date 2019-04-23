18d6c82c986d3fccab3fd0442ba2369643d31a71
VladOsiichuk <vladdosiichuk@gmail.com>
2019-04-23 08:33:47 +0300

    Heroku configuring

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -227,3 +229,4 @@ DEBUG_TOOLBAR_PANELS = [
 SHOW_TOOLBAR_CALLBACK = True
 
 INTERNAL_IPS = ('127.0.0.1',)
+django_heroku.settings(locals())
