c9d16f3f0d7c011a3f0628b28f3f2d0b02f5ace0
VladOsiichuk <vladdosiichuk@gmail.com>
2019-05-01 21:58:30 +0300

    adding csrf_exempt

--- api_rules/urls.py
+++ api_rules/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from .views import SetUsersPermissionsAPIView, UpdateUsersPermissionsAPIView
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path("rules/", csrf_exempt(SetUsersPermissionsAPIView.as_view())),
-    path("rules/update/", csrf_exempt(UpdateUsersPermissionsAPIView.as_view())),
+    path("rules/", SetUsersPermissionsAPIView.as_view()),
+    path("rules/update/", UpdateUsersPermissionsAPIView.as_view()),
 ]
--- desk/api_comments/urls.py
+++ desk/api_comments/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from .views import CommentAPIView, CreateCommentAPIView
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path("comments/", csrf_exempt(CommentAPIView.as_view())),
-    path("comments/create/", csrf_exempt(CreateCommentAPIView.as_view()))
+    path("comments/", CommentAPIView.as_view()),
+    path("comments/create/", CreateCommentAPIView.as_view())
 ]
