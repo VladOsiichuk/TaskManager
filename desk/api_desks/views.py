947e05540e2b184b018610a1e3c48cf2f7791329
VladOsiichuk <vladdosiichuk@gmail.com>
2020-02-24 19:18:04 +0200

    Update desk create response

--- desk/api_desks/views.py
+++ desk/api_desks/views.py
@@ -63,6 +63,8 @@ class DeskAPIView(generics.ListAPIView,
         # create Desk object
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
+
 
         desk_object = self.perform_create(serializer)
 
